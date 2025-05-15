87872bbf7e8bb7b4ba738b8376017ea0c4a4cb09
YuriyTabinskuy <118268946+YuriyTabinskuy@users.noreply.github.com>
2025-05-15 23:32:16 +0300

    добавив 4 і 5 роботу

--- src/test/java/SortPack/QuickSortTest.java
+++ src/test/java/SortPack/QuickSortTest.java
@@ -1,5 +1,6 @@
+package SortPack;
+
 import com.example.QuickSort;
-import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
